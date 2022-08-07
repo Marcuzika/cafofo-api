747f9133ae22707018fc852021e3023f5027ee9d
Sérgio <serjoca.dev@gmail.com>
2022-08-07 17:25:38 -0300

    feat(app): add app product endpoints

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,13 +1,31 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Post, Put } from '@nestjs/common';
 
 import { AppService } from './app.service';
 
-@Controller()
+@Controller('/app')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('/products')
+  getProducts() {
+    return this.appService.getProducts();
+  }
+
+  @Post('/products')
+  postProducts(@Body() body) {
+    const product = this.appService.addProduct(body);
+    return product;
+  }
+
+  @Put('/products')
+  putProducts(@Body() body) {
+    const product = this.appService.updateProduct(body);
+    return product;
+  }
+
+  @Delete('/products')
+  deleteProducts(@Body() body) {
+    const products = this.appService.removeProduct(body);
+    return products;
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,47 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  private readonly products = [
+    {
+      name: 'Razer 5000',
+      price: '500.00',
+      currency: 'R$',
+    },
+  ];
+
+  getProducts() {
+    return this.products;
+  }
+
+  addProduct(body) {
+    const { name, price, currency } = body;
+
+    this.products.push({
+      name,
+      price,
+      currency,
+    });
+
+    return this.products[this.products.length - 1];
+  }
+
+  updateProduct(body) {
+    const { id, name, price, currency } = body;
+
+    this.products[id] = {
+      name,
+      price,
+      currency,
+    };
+
+    return this.products[id];
+  }
+
+  removeProduct(body) {
+    const { id } = body;
+
+    delete this.products[id];
+
+    return this.products;
   }
 }
